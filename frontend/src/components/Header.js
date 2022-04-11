faf5bad88be61caec37f64daa9f8d9bcf97581d4
Antoine Lemarchand <lemarchand.antoine00@gmail.com>
2022-04-12 00:49:42 +0200

    lancement du frontend - épuration de certain fichier react - démarrage du styles

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const headers = () => {
+  const JSX = (
+    <div className="header-container">
+      <h1>Groupomania</h1>
+      <div className="header-container_link">
+        <i className="fa-solid fa-house"></i>
+        <i className="fa-solid fa-user"></i>
+        <i className="fa-solid fa-user-group"></i>
+        <i className="fa-solid fa-arrow-right-from-bracket"></i>
+      </div>
+    </div>
+  );
+  return JSX;
+
+  // test de commentaire JS
+};
+
+export default headers;
